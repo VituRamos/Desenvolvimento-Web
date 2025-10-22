7e5e56a4e46d01d8d933f1804ed688a164734b58
Lucas Viana <55816219+Lucas-vna@users.noreply.github.com>
2025-10-22 15:58:28 -0300

    Delete ATV2/Front-End directory

--- ATV2/Front-End/src/Components/Actions.jsx
+++
@@ -1,7 +0,0 @@
-export default function Actions({ onVoltar }) {
-  return (
-    <div className="actions">
-      <button className="btn" onClick={onVoltar}>Voltar</button>
-    </div>
-  );
-}
--- ATV2/Front-End/src/Components/FeedbackItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function FeedbackItem({ titulo, texto }) {
-  return (
-    <div className="feedback-item">
-      <h4>{titulo}</h4>
-      <p>{texto}</p>
-    </div>
-  );
-}
